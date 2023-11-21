3dbe8c9b68c3cd38c65518dfaf4166418a554668
Rio Saputro <rio.ae23@gmail.com>
2023-11-21 19:38:18 +0700

    Higher Order Function & File Export

---
+++ batako.js
@@ -0,0 +1,11 @@
+const sumNumber = (a, b) => {  
+    return a + b;
+    }
+const squareNumber = (a) => {
+    return a * a;
+}
+
+module.exports = {
+    sumNumber,
+    squareNumber
+}
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const {promisify} = require('util')
+const {batamu, sumNumber} = require('./batako')
 
 function getUsers(arr, cb){
     setTimeout(()=>{
@@ -16,6 +17,12 @@ getUsersPromise(['joe','john','jane'])
 .then(userCall)
 .catch(err=>console.log(err))
 
+const promiseMe = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve('Success')
+    },2000)
+})
+
 testAsync = () =>{
     setTimeout(()=>{
         console.log('test')
@@ -23,3 +30,15 @@ testAsync = () =>{
 }
 testAsync();
 testAsync();
+
+const number = [1,2,3,4,5,6,7,8,9,10]
+const even = number.filter( n => {
+    return n%2 === 0;
+})
+console.log(even)
+const m = number.map( n => {
+    return {number : n*2};
+})
+console.log(m)
+
+console.log(sumNumber(1,2));
