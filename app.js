dc11fe33a8c56dde81342ab1a03584bc3f3e684f
Rio Saputro <rio.ae23@gmail.com>
2023-11-26 20:06:56 +0700

    Test Deploy

--- app.js
+++ app.js
@@ -1,29 +1,41 @@
-const express = require('express');
-const fs = require('fs');
+const express = require("express");
+const fs = require("fs");
 const app = express();
 const port = 3000;
 
-app.set('view engine', 'ejs');
-app.use(express.static('views'));
+app.set("view engine", "ejs");
+app.use(express.static("views"));
 
+const dataArr = [
+//   {
+//     name: "John",
+//     age: 25,
+//   },
+//   {
+//     name: "Jane",
+//     age: 24,
+//   },
+//   {
+//     name: "Jack",
+//     age: 26,
+//   },
+];
 
-app.get('/', (req, res) => {
-    // res.sendFile('./public/index.html', { root: __dirname });
-    res.render('index', { title: 'Home' });
+app.get("/", (req, res) => {
+  // res.sendFile('./public/index.html', { root: __dirname });
+  res.render("index", { title: "Home", dataArr });
 });
 
-app.get('/data/:id', (req, res) => {
-    const id = req.params.id;
-    res.render('data', { title: 'Data', id: id });
+app.get("/data/:id", (req, res) => {
+  const id = req.params.id;
+  res.render("data", { title: "Data", id: id, dataArr });
 });
 
-
-app.use('/', (req, res) => {
-    res.status(404);
-    res.send('<h1>404</h1>');
-})
-
+app.use("/", (req, res) => {
+  res.status(404);
+  res.send("<h1>404</h1>");
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
