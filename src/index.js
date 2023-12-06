a2ac34a0da6e420de311f1ff7ec48060a70d3ef3
Rio Saputro <rio.ae23@gmail.com>
2023-12-06 13:28:18 +0700

    rapiin

--- src/index.js
+++ src/index.js
@@ -14,14 +14,13 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-
 app.use('/user', userRoutes);
 app.use('/post', postRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("Sorry can't find that!");
+  res.status(404).send("Page not found!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`listening at http://localhost:${port}`);
 });
