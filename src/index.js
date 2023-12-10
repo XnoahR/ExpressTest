90f9cea574a7ae287f7d12d4808243c913da77e9
Rio Saputro <rio.ae23@gmail.com>
2023-12-10 17:26:49 +0700

    Connect Bucket done

--- src/index.js
+++ src/index.js
@@ -5,24 +5,44 @@ import user from "./models/userModel.js";
 import animal from "./models/animalModel.js";
 import post from "./models/postModel.js";
 import sequelize from "./utils/db.js";
-
-
+// import bucket from @google-cloud/storage;
+import { Storage } from "@google-cloud/storage";
 
 import userRoutes from "./routes/userRoute.js";
 import postRoutes from "./routes/postRoute.js";
 import authRoutes from "./routes/authRoute.js";
 import authMiddleware from "./middlewares/authMiddleware.js";
 
+const storage = new Storage({
+  projectId: "petme-406008",
+  credentials: {
+    type: process.env.CREDENTIAL_TYPE,
+    project_id: process.env.CREDENTIAL_PROJECT_ID,
+    private_key_id: process.env.CREDENTIAL_PRIVATE_KEY_ID,
+    private_key: process.env.CREDENTIAL_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.CREDENTIAL_CLIENT_EMAIL,
+    client_id: process.env.CREDENTIAL_CLIENT_ID,
+    auth_uri: process.env.CREDENTIAL_AUTH_URI,
+    token_uri: process.env.CREDENTIAL_TOKEN_URI,
+    auth_provider_x509_cert_url:
+      process.env.CREDENTIAL_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.CREDENTIAL_CLIENT_X509_CERT_URL,
+  },
+});
 
+// const storage = new Storage({
+//   projectId: "petme-406008",
+//   keyFilename: process.env.KEY_FILE_PATH,
+// });
+const bucket = storage.bucket("petmebucket");
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use('/user',authMiddleware, userRoutes);
-app.use('/post',authMiddleware, postRoutes);
-app.use('/account', authRoutes);
-
+app.use("/user", authMiddleware, userRoutes);
+app.use("/post", authMiddleware, postRoutes);
+app.use("/account", authRoutes);
 
 app.get("/admin", (req, res) => {
   user.findOrCreate({
@@ -35,6 +55,18 @@ app.get("/admin", (req, res) => {
     },
   });
   res.send("Admin created");
+});
+
+app.get("/bucket", async (req, res) => {
+  try{
+    const [files] = await bucket.getFiles();
+    files.forEach((file) => {
+      console.log(file.name);
+    });
+    res.send(files);
+  } catch(err){
+    res.send(err.message);
+  }
 });
 
 app.use((req, res, next) => {
