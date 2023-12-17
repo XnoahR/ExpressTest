6b341c416ed9311d9a79eca8d66565d25a2e0020
Rio Saputro <rio.ae23@gmail.com>
2023-12-17 14:50:04 +0700

    Notification MRC

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
 
 import fileUpload from "express-fileupload";
 
@@ -7,8 +8,7 @@ import userRoutes from "./routes/userRoute.js";
 import postRoutes from "./routes/postRoute.js";
 import authRoutes from "./routes/authRoute.js";
 import authMiddleware from "./middlewares/authMiddleware.js";
-
-
+import notificatttionRoute from "./routes/notificationRoute.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -18,6 +18,7 @@ app.use(fileUpload());
 app.use("/user", authMiddleware, userRoutes);
 app.use("/post", authMiddleware, postRoutes);
 app.use("/account", authRoutes);
+app.use("/notification", notificatttionRoute);
 
 
 app.use((req, res, next) => {
